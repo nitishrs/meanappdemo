0f05e7ed7333fc699e7f505d15666ca3eb3f84f7
Nitish <nitishrs@gmail.com>
2016-01-22 11:54:27 +0530

    Adding passport local validation

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,5 +1,11 @@
-angular.module('app').controller('mvNavBarLoginCtrl', function ($scope){
-    $scope.signin = function(username, password) {
-        console.log("Working on the Login function");
+angular.module('app').controller('mvNavBarLoginCtrl', function ($scope, $http){
+    $scope.signin = function(username, password){
+        $http.post('/login', {username: username, password: password}).then(function(response){
+            if(response.data.success) {
+                console.log('Logged in');
+            } else {
+                console.log('Failed to log in');
+            }
+        })
     }
 });
--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -7,4 +7,20 @@ module.exports = function(config) {
     db.once('open', function callback() {
         console.log('Multivision DB Opened...');
     });
+
+    var userSchema = mongoose.Schema({
+        firstName: String,
+        lastName: String,
+        username: String
+    });
+
+    var User = mongoose.model('User', userSchema);
+
+    User.find({}).exec(function(err, collection) {
+        if(collection.length === 0) {
+            User.create({firstName: 'Nitish', lastName: 'Shiggaon', username: 'nitishrs'});
+            User.create({firstName: 'Kantesh', lastName: 'Kulkarni', username: 'kanteshyk'});
+            User.create({firstName: 'Sunith', lastName: 'K S', username: 'sunithks'});
+        }
+    })
 };
